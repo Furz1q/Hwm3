5f99f1132610513888511f3f841d0c720b32a8bd
Furz1q <serbek200@mail.ru>
2024-09-01 01:24:42 +0300

    Конец

--- src/Main.java
+++ src/Main.java
@@ -14,11 +14,11 @@ public class Main {
         }
         System.out.println("Задание 2");
 
-        int temperature = -5;
+        int temperature = -6;
         if (temperature < 5){
-            System.out.println("На улицее холодно нужно надеть шапку ");
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
         }else{
-            System.out.println("Сегодня тепло, можно идти без шапки");
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
         }
         System.out.println("Задние 3");
 
@@ -31,40 +31,44 @@ public class Main {
         System.out.println("Задание 4");
 
         int agePeople = 23;
-        if ( agePeople >= 2 && agePeople <= 6){
+        if (agePeople >= 2 && agePeople <= 6){
             System.out.println("Если возраст человека равен " + agePeople + " то ему нужно ходить в детский сад");
         }
-        if ( agePeople >= 7 && agePeople <= 17){
+        if (agePeople >= 7 && agePeople <= 17){
             System.out.println("Если возраст человека равен " + agePeople + " то ему нужно ходить в в школу");
         }
-        if ( agePeople >= 18 && agePeople <= 24){
+        if (agePeople >= 18 && agePeople <= 24){
             System.out.println("Если возраст человека равен " + agePeople + " то его место в университете");
         }
-        if ( agePeople > 24){
+        if (agePeople > 24){
             System.out.println("Если возраст человека равен " + agePeople + " то ему пора ходить на работу");
         }
         System.out.println("Задание 5");
 
         int ageChild = 14;
-        if( ageChild <= 5){
+        if (ageChild < 5){
             System.out.println("Если возраст ребенка равен " + ageChild + " то ему нельзя кататься на аттракционе");
         }
-        else if( ageChild > 5 && ageChild < 14){
+        if (ageChild >= 5 && ageChild < 14){
             System.out.println("Если возраст ребенка равен " +
                     ageChild + " то ему можно кататься на аттракционе в сопровождении взрослого");
         }
-        else{
+        if (ageChild >= 14 ){
             System.out.println("Если возраст ребенка равен " +
                     ageChild + " то ему можно кататься на аттракционе без сопровождения взрослого");
         }
         System.out.println("Задание 6");
 
-        int wagonСapacity = 77;
-        if ( wagonСapacity <= 60){
+        int places = 102;
+        int sitPlaces = 60;
+        int people = 80;
+        if (people < sitPlaces){
             System.out.println("Есть сидячие места");
-        } else if ( wagonСapacity > 60 && wagonСapacity <= 102){
+        }
+        if (people >= sitPlaces && people < places){
             System.out.println("Есть стоячие места");
-        } else {
+        }
+        if (people >= places){
             System.out.println("Вагон уже полностью забит");
         }
         System.out.println("Задание 7");
@@ -72,12 +76,12 @@ public class Main {
         int one = 25;
         int two = 104;
         int three = -74;
-        if ( one > two && one > three){
-            System.out.println(one);
-        }else if ( two > three){
-            System.out.println(two);
+        if (one >= two && one >= three){
+            System.out.println("Максимольное число в переменной one, равно " + one);
+        }else if (two >= three){
+            System.out.println("Максимольное число в переменной two, равно " + two);
         }else{
-            System.out.println(three);
+            System.out.println("Максимольное число в переменной three, равно " + three);
         }
     }
 }
